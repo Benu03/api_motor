519d24099a42fa6867df17f6da81056f26391fe3
Your Name <ibnu.khoirin03@gmail.com>
2025-05-26 12:05:04 +0700

    invoice user

--- app/Http/Controllers/RiwayatController.php
+++ app/Http/Controllers/RiwayatController.php
@@ -129,7 +129,7 @@ class RiwayatController extends Controller
 
                 ];
             }),
-            'pdf_url' => url('api/v1/user/invoice-pdf/' . $dataH->invoice_no, [], true)
+            'pdf_url' => request()->getSchemeAndHttpHost() . '/api/v1/user/invoice-pdf/' . $dataH->invoice_no
         ];
     
         Log::info('End Notif InvoiceGenerate');
