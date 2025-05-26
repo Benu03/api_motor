eb9220847b09785a3d09a0ab72d3911a24130f2e
Your Name <ibnu.khoirin03@gmail.com>
2025-05-26 11:59:51 +0700

    invoice user

--- routes/mtr.php
+++ routes/mtr.php
@@ -15,8 +15,8 @@
 
 
 
-// $mtr->group(['middleware' => 'key_service'], function () use ($mtr) 
-// {
+$mtr->group(['middleware' => 'key_service'], function () use ($mtr) 
+{
         $mtr->group(['prefix' => 'api/'], function () use ($mtr) 
         {
             
@@ -99,6 +99,6 @@
             });
         });
 
-// });
+});
 
 $mtr->get('api/v1/get-image-service-detail/{data}', 'ReportController@GetImageServiceDetail');
